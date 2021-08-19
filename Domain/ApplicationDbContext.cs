53a49f35e7121118c2e4bcc2ad19a734298d7385
Egor <sychyov1991@mail.ru>
2021-08-19 12:13:55 +0600

    chore(): Added EF Core

---
+++ Domain/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sigmade.Domain
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
